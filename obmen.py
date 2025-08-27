ef341dacf03cc34659edb78ccea41800d337e696
kseny1992 <xen_andeeva@bk.ru>
2025-08-27 12:10:23 +0300

    создана функция excange для получения курсов валют

--- obmen.py
+++ obmen.py
@@ -3,9 +3,25 @@ import json
 from tkinter import *
 from tkinter import messagebox as mb
 
-from PyInstaller.loader.pyiboot01_bootstrap import entry
+from bottle import response
+
 
 def exchange():
+    code = entry.get()
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data ['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo('Курс обмена', f'Курс:{exchange_rate} {code} за 1 доллар')
+            else:
+                mb.showerror("Ошибка",f'Валюта {code} не найдена')
+        except Exception as e:
+            mb.showerror("Ошибка",f"Произошла ошибка:{e}.")
+    else:
+        mb.showwarning("Внимание!","Введите код валюты") #ПРЕДУПРЕЖДЕНИЕ
 
 
 window = Tk()
